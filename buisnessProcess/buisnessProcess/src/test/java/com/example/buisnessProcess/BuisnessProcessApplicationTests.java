7da0baad9e2afa580ad2e02467d3d398dec503f9
Rickycst <creusot.aymeric7@gmail.com>
2020-04-01 09:32:30 +0200

    first commit

---
+++ buisnessProcess/buisnessProcess/src/test/java/com/example/buisnessProcess/BuisnessProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buisnessProcess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuisnessProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
